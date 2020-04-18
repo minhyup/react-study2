b1235858c0e36ad6f844a9d75a38e66e4d6fda4a
kang <minhyupp@gmail.com>
2020-04-18 16:21:35 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -4,61 +4,103 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+
+  return array;
+}
+
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
+  //const nextId = useRef(2501);
   const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    (text) => {
-      console.log('text::', text);
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+    // setTodos((todos) => {
+    //   todos.map((todo) => {
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //   })
+    // })
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  // const onInsert = useCallback(
+  //   (text) => {
+  //     console.log('text::', text);
+
+  //     const todo = {
+  //       id: nextId.current,
+  //       text,
+  //       checked: false,
+  //     };
+  //     setTodos(todos.concat(todo));
+  //     nextId.current += 1;
+  //   },
+  //   [todos],
+  // );
+
+  // const onRemove = useCallback(
+  //   (id) => {
+  //     setTodos(todos.filter((todo) => todo.id !== id));
+  //   },
+  //   [todos],
+  // );
+
+  // const onToggle = useCallback(
+  //   (id) => {
+  //     setTodos(
+  //       todos.map((todo) =>
+  //         todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+  //       ),
+  //     );
+  //   },
+  //   [todos],
+  // );
 
   return (
     <TodoTemplate>
